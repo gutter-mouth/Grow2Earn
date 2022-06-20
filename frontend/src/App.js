33d28d57f1a83e0533c8d50f4649350dabaed53f
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2022-06-20 17:06:53 +0900

    enable mint from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 // import { etherscan } from "../../nft-collectible/hardhat.config";
-import {ethers } from "ethers";
+import { ethers } from "ethers";
 import './App.css';
 import { CONTRACT_ADDRESS } from "./constants";
-import contract from "./contracts/NFTCollectible.json"
+import { FileUpload } from 'react-ipfs-uploader'
+import contract from "./contracts/Grow2Earn.json"
 const abi = contract.abi;
 
 function App() {
-  
+
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [fileUrl, setFileUrl] = useState('')
 
 
   const checkWalletIsConnected = async () => {
@@ -36,7 +38,7 @@ function App() {
       return;
     } else {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-      
+
       if (accounts.length !== 0) {
         console.log("Connected to: ", accounts[0]);
         setCurrentAccount(accounts[0])
@@ -47,27 +49,28 @@ function App() {
     }
   };
 
-  const mintNftHandler = async() => {
+  const mintNftHandler = async () => {
     try {
-      const {ethereum} = window;
-      if(ethereum){
+      console.log(fileUrl);
+      const { ethereum } = window;
+      if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const nftContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
         console.log("Start minting");
-        let txn = await nftContract.mintNFTs(1, {value: ethers.utils.parseEther("0.01")});
+        let txn = await nftContract.makeAgaveNFT(fileUrl);
         console.log("Minting");
         await txn.wait();
         console.log("Minted");
-      } else{
+      } else {
         console.log("No ethereum object.");
       }
     }
-    catch(error){
+    catch (error) {
       console.log(error);
     }
-   };
+  };
 
   const connectWalletButton = () => {
     return (
@@ -80,11 +83,29 @@ function App() {
     );
   };
 
+
+  const uploadImageButton = () => {
+    return (
+      <div>
+        <FileUpload setUrl={setFileUrl} />
+        FileUrl : <a
+          href={fileUrl}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {fileUrl}
+        </a>
+      </div>
+    );
+  }
+
   const mintNftButton = () => {
     return (
+
       <button onClick={mintNftHandler} className="cta-button mint-nft-button">
         Mint NFT
       </button>
+
     );
   };
 
@@ -94,14 +115,16 @@ function App() {
 
   return (
     <div className="main-app">
-      <h1>Scrappy Squirrels Tutorial</h1>
+      <h1>Agave NFT</h1>
       {!currentAccount && (
         <div>{connectWalletButton()}</div>
       )}
-      {currentAccount && (
+      {currentAccount && fileUrl == "" && (
+        <div>{uploadImageButton()}</div>
+      )}
+      {currentAccount && fileUrl != "" && (
         <div>{mintNftButton()}</div>
       )}
-      
     </div>
   );
 }
